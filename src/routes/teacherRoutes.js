e462a9bcb827aff816149825cd03ef90b89dedf8
Sameer kadyan <gulshansameer51@gmail.com>
2026-06-11 22:38:34 +0530

    fix again

--- src/routes/teacherRoutes.js
+++ src/routes/teacherRoutes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
+const authorizeRoles = require("../middleware/roleMiddleware");
 const getTeacherProfileController = require("../controllers/teacher/getTeacherProfileController");
 
 router.get(
   "/profile",
   authMiddleware,
   authorizeRoles("teacher"),
-  getProfile
+  getTeacherProfileController
 );
 
 module.exports = router;
