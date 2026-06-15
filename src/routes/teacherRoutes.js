4a6e73144d60d7cd311da271d6aa4e92c5b70fef
Sameer kadyan <gulshansameer51@gmail.com>
2026-06-15 06:33:54 +0530

    teacherRoutes fix

--- src/routes/teacherRoutes.js
+++ src/routes/teacherRoutes.js
@@ -5,22 +5,22 @@ const authMiddleware = require("../middleware/authMiddleware");
 const authorizeRoles = require("../middleware/roleMiddleware");
 const getTeacherProfileController = require("../controllers/teacher/getTeacherProfileController");
 
-// router.get(
-//   "/profile",
-//   authMiddleware,
-//   authorizeRoles("teacher"),
-//   getTeacherProfileController
-// );
-
 router.get(
   "/profile",
   authMiddleware,
-  (req, res) => {
-    res.json({
-      success: true,
-      user: req.user,
-    });
-  }
+  authorizeRoles("teacher"),
+  getTeacherProfileController
 );
+
+// router.get(
+//   "/profile",
+//   authMiddleware,
+//   (req, res) => {
+//     res.json({
+//       success: true,
+//       user: req.user,
+//     });
+//   }
+// );
 
 module.exports = router;
